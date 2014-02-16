fcc03ce333cabb3b93ae51f4a961a8eb8f21dcac
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2014-02-15 22:17:13 -0600

    file copyright

--- src/org/opensilk/music/cast/CastWebServer.java
+++ src/org/opensilk/music/cast/CastWebServer.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2014 OpenSilk Productions LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.opensilk.music.cast;
 
 import android.content.Context;
@@ -27,7 +42,6 @@ import fi.iki.elonen.NanoHTTPD;
 import hugo.weaving.DebugLog;
 
 /**
- * Created by drew on 2/14/14.
  * Based on SimpleWebServer from NanoHttpd
  *
  * Serves audio files and album art
@@ -35,6 +49,8 @@ import hugo.weaving.DebugLog;
  *      /audio/${id}
  *      /art/${id}
  * where id is value of audio._id of the track in the MediaStore
+ *
+ * Created by drew on 2/14/14.
  */
 public class CastWebServer extends NanoHTTPD {
     /**
