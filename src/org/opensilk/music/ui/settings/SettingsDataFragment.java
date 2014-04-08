1f4cc18449e7aa22d0935ae72fd7f53f6c1d8e25
Andrew Boren <andrew.boren@gmail.com>
2014-04-07 20:13:40 -0700

    Update cache size setting on demand.
    Change-Id: I6d9ddced0ad2292f19b9ce93293c5d1ad1ee149f

--- src/org/opensilk/music/ui/settings/SettingsDataFragment.java
+++ src/org/opensilk/music/ui/settings/SettingsDataFragment.java
@@ -3,11 +3,11 @@ package org.opensilk.music.ui.settings;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.widget.Toast;
 
 import com.andrew.apollo.R;
-import com.andrew.apollo.utils.ApolloUtils;
 import com.andrew.apollo.utils.PreferenceUtils;
 
 import org.apache.commons.io.FileUtils;
@@ -23,20 +23,33 @@ import static org.opensilk.music.artwork.ArtworkManager.DISK_CACHE_DIRECTORY;
  */
 public class SettingsDataFragment extends SettingsFragment {
 
+    private ListPreference mCacheSize;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.settings_data);
-        setupCacheSize();
+
+
+        mCacheSize = (ListPreference) findPreference(PreferenceUtils.PREF_CACHE_SIZE);
+        mCacheSize.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                setCacheSizeSummary(Integer.valueOf((String) newValue));
+                return true;
+            }
+        });
+
+        setCacheSizeSummary(PreferenceUtils.getInstance(
+                getActivity()).imageCacheSizeBytes() / 1024 / 1024);
         setupDeleteCache();
     }
 
-    private void setupCacheSize() {
-        final Preference cacheSize = findPreference("pref_cache_size");
-        if (cacheSize != null) {
-            cacheSize.setSummary(String.format(Locale.US, "%.02f/%d MB",
-                    (float) FileUtils.sizeOfDirectory(CacheUtil.getCacheDir(getActivity(), DISK_CACHE_DIRECTORY)) / 1024 / 1024,
-                    PreferenceUtils.getInstance(getActivity()).imageCacheSizeBytes() / 1024 / 1024));
+    private void setCacheSizeSummary(int size) {
+        if (mCacheSize != null) {
+            mCacheSize.setSummary(String.format(Locale.US, "%.02f/%d MB",
+                    (float) FileUtils.sizeOfDirectory(CacheUtil.getCacheDir(
+                    getActivity(),DISK_CACHE_DIRECTORY)) / 1024 / 1024, size));
         }
     }
 
