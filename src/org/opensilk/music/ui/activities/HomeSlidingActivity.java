ac18cc2a5b2dd0748d836258979a7029723ae8ba
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2014-07-01 19:56:12 -0500

    dont open the drawer on first launch
    doesnt work, just closes right away, dont want to fuck with it
    
    Change-Id: Ia34d93684a2d9c5d18d9dbce28162293ba80be7f

--- src/org/opensilk/music/ui/activities/HomeSlidingActivity.java
+++ src/org/opensilk/music/ui/activities/HomeSlidingActivity.java
@@ -75,12 +75,6 @@ public class HomeSlidingActivity extends BaseSlidingActivity implements
     private static final String STATE_SELECTED_POSITION = "selected_navigation_drawer_position";
 
     /**
-     * Per the design guidelines, you should show the drawer on launch until the user manually
-     * expands it. This shared preference tracks this.
-     */
-    private static final String PREF_USER_LEARNED_DRAWER = "navigation_drawer_learned";
-
-    /**
      * Helper component that ties the action bar to the navigation drawer.
      */
     private ActionBarDrawerToggle mDrawerToggle;
@@ -95,7 +89,6 @@ public class HomeSlidingActivity extends BaseSlidingActivity implements
     private ArrayAdapter<PluginInfo> mDrawerAdapter;
 
     private int mCurrentSelectedPosition = 0;
-    private boolean mUserLearnedDrawer;
     private CharSequence mTitle;
 
     @Inject
@@ -111,11 +104,6 @@ public class HomeSlidingActivity extends BaseSlidingActivity implements
 
         // cancel any pending clear cache request
         mArtworkService.cancelCacheClear();
-
-        // Read in the flag indicating whether or not the user has demonstrated awareness of the
-        // drawer. See PREF_USER_LEARNED_DRAWER for details.
-        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
-        mUserLearnedDrawer = sp.getBoolean(PREF_USER_LEARNED_DRAWER, false);
 
         // Init drawer adapter
         // Add default plugin now since we know it will always be there
@@ -163,27 +151,11 @@ public class HomeSlidingActivity extends BaseSlidingActivity implements
             @Override
             public void onDrawerOpened(View drawerView) {
                 super.onDrawerOpened(drawerView);
-
-                if (!mUserLearnedDrawer) {
-                    // The user manually opened the drawer; store this flag to prevent auto-showing
-                    // the navigation drawer automatically in the future.
-                    mUserLearnedDrawer = true;
-                    SharedPreferences sp = PreferenceManager
-                            .getDefaultSharedPreferences(HomeSlidingActivity.this);
-                    sp.edit().putBoolean(PREF_USER_LEARNED_DRAWER, true).apply();
-                }
-
                 supportInvalidateOptionsMenu(); // calls onPrepareOptionsMenu()
             }
         };
 
         mDrawerLayout.setDrawerListener(mDrawerToggle);
-
-        // If the user hasn't 'learned' about the drawer, open it to introduce them to the drawer,
-        // per the navigation drawer design guidelines.
-        if (!mUserLearnedDrawer) {
-            mDrawerLayout.openDrawer(mDrawerContainerView);
-        }
 
         // Defer code dependent on restoration of previous instance state.
         mDrawerLayout.post(new Runnable() {
