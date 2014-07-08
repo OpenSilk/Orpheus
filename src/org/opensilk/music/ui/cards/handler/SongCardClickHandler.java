6ee838a01bafe6faa5de953f282115e37dff4293
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2014-07-08 11:06:39 -0500

    implement song card click handler
    Change-Id: I8d2d4560dd5075613da429d3cd8c270a640ffc97

--- src/org/opensilk/music/ui/cards/handler/SongCardClickHandler.java
+++ src/org/opensilk/music/ui/cards/handler/SongCardClickHandler.java
@@ -18,6 +18,15 @@ package org.opensilk.music.ui.cards.handler;
 
 import android.support.v4.app.FragmentActivity;
 
+import com.andrew.apollo.menu.DeleteDialog;
+import com.andrew.apollo.model.LocalSong;
+import com.andrew.apollo.utils.MusicUtils;
+import com.andrew.apollo.utils.NavUtils;
+import com.squareup.otto.Subscribe;
+
+import org.opensilk.music.api.model.Song;
+import org.opensilk.music.dialogs.AddToPlaylistDialog;
+import org.opensilk.music.ui.cards.event.SongCardClick;
 import org.opensilk.silkdagger.qualifier.ForActivity;
 
 import javax.inject.Inject;
@@ -36,4 +45,46 @@ public class SongCardClickHandler {
     public FragmentActivity getActivity() {
         return activity;
     }
+
+    @Subscribe
+    public void onSongCardEvent(SongCardClick e) {
+        switch (e.event) {
+            case PLAY:
+                MusicUtils.playAllSongs(getActivity(), new Song[]{e.song}, 0, false);
+                break;
+            case PLAY_NEXT:
+                MusicUtils.playNext(getActivity(), new Song[]{e.song});
+                break;
+            case ADD_TO_QUEUE:
+                MusicUtils.addSongsToQueue(getActivity(), new Song[]{e.song});
+                break;
+            case ADD_TO_PLAYLIST:
+                if (e.song instanceof LocalSong) {
+                    LocalSong song = (LocalSong) e.song;
+                    AddToPlaylistDialog.newInstance(new long[]{song.songId})
+                            .show(getActivity().getSupportFragmentManager(), "AddToPlaylistDialog");
+                }
+                break;
+            case MORE_BY_ARTIST:
+                if (e.song instanceof LocalSong) {
+                    LocalSong song = (LocalSong) e.song;
+                    NavUtils.openArtistProfile(getActivity(), MusicUtils.makeArtist(getActivity(), song.artistName));
+                }
+                break;
+            case SET_RINGTONE:
+                if (e.song instanceof LocalSong) {
+                    LocalSong song = (LocalSong) e.song;
+                    MusicUtils.setRingtone(getActivity(), song.songId);
+                }
+                break;
+            case DELETE:
+                if (e.song instanceof LocalSong) {
+                    LocalSong song = (LocalSong) e.song;
+                    DeleteDialog.newInstance(song.name, new long[]{song.songId}, null)
+                            .show(getActivity().getSupportFragmentManager(), "DeleteDialog");
+                }
+                break;
+        }
+    }
+
 }
