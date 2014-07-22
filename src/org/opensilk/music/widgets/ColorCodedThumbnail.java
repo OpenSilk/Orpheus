67d77e41b0959a7f6323dc6c70e9b93629e8717a
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2014-07-22 12:44:32 -0500

    fix weirdness with list card thumbs
    Change-Id: I2e989d1db5d861484a1fe29a186b558c2fb99f21

--- src/org/opensilk/music/widgets/ColorCodedThumbnail.java
+++ src/org/opensilk/music/widgets/ColorCodedThumbnail.java
@@ -44,8 +44,33 @@ public class ColorCodedThumbnail extends TextView {
     }
 
     @Override
-    public void setBackgroundResource(int resid) {
-        super.setBackgroundResource(resid);
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        final int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        final int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+
+        if (widthSize == 0 && heightSize == 0) {
+            // If there are no constraints on size, let FrameLayout measure
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+            // Now use the smallest of the measured dimensions for both dimensions
+            final int minSize = Math.min(getMeasuredWidth(), getMeasuredHeight());
+            setMeasuredDimension(minSize, minSize);
+            return;
+        }
+
+        final int size;
+        if (widthSize == 0 || heightSize == 0) {
+            // If one of the dimensions has no restriction on size, set both dimensions to be the
+            // on that does
+            size = Math.max(widthSize, heightSize);
+        } else {
+            // Both dimensions have restrictions on size, set both dimensions to be the
+            // smallest of the two
+            size = Math.min(widthSize, heightSize);
+        }
+
+        final int newMeasureSpec = MeasureSpec.makeMeasureSpec(size, MeasureSpec.EXACTLY);
+        super.onMeasure(newMeasureSpec, newMeasureSpec);
     }
 
     public void init(String title) {
