f8f7043a6c8368c0b5b497a51aca9c369062e866
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2014-10-31 20:03:37 -0500

    cleanup
    Change-Id: I0d0e81e4f949df5ec4ef6a233a6f3b61fce43107

--- src/org/opensilk/music/ui2/core/CanShowScreen.java
+++
@@ -1,9 +0,0 @@
-
-package org.opensilk.music.ui2.core;
-
-import flow.Flow;
-import mortar.Blueprint;
-
-public interface CanShowScreen<S extends Blueprint> {
-    void showScreen(S screen, Flow.Direction direction);
-}
