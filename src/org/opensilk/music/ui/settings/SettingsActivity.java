b827f3373ceff256e627f4427744da0e4d6264cc
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2014-11-08 11:50:19 -0600

    fix settings
    Change-Id: I0609dffcbc278cb4b2c77ef4b76b4da1561fce0b

--- src/org/opensilk/music/ui/settings/SettingsActivity.java
+++ src/org/opensilk/music/ui/settings/SettingsActivity.java
@@ -3,6 +3,7 @@ package org.opensilk.music.ui.settings;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import org.opensilk.music.R;
 
@@ -11,22 +12,29 @@ import org.opensilk.music.ui.activities.ActivityModule;
 import org.opensilk.music.ui2.BaseActivity;
 import org.opensilk.silkdagger.DaggerInjector;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
 /**
  * Created by andrew on 2/28/14.
  */
 public class SettingsActivity extends BaseActivity {
 
+    @InjectView(R.id.main_toolbar) Toolbar mToolbar;
+
     @Override
     public void onCreate(final Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        ((DaggerInjector) getApplication()).getObjectGraph().plus(getModules()).inject(this);
-
-        setContentView(R.layout.blank_framelayout);
-
         boolean light = getIntent().getBooleanExtra(OrpheusApi.EXTRA_WANT_LIGHT_THEME, false);
         setTheme(light ? R.style.Theme_Settings_Light : R.style.Theme_Settings_Dark);
 
+        ((DaggerInjector) getApplication()).getObjectGraph().plus(getModules()).inject(this);
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.blank_framelayout_toolbar);
+        ButterKnife.inject(this);
+
+        setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         if (savedInstanceState == null) {
