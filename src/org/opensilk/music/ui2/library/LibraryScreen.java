6709ecf6fff21d1eebe669156beb2d82982b57bb
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2014-11-26 15:14:45 -0600

    fix drawer on library
    Change-Id: Ia1c9a757877d10089455b4d43bee2dd667a4a958

--- src/org/opensilk/music/ui2/library/LibraryScreen.java
+++ src/org/opensilk/music/ui2/library/LibraryScreen.java
@@ -299,7 +299,6 @@ public class LibraryScreen extends Screen {
         void setupActionBar() {
             actionBarOwner.setConfig(
                     new ActionBarOwner.Config.Builder()
-                            .setUpButtonEnabled(true)
                             .setTitle(pluginInfo.title)
                             .setSubtitle(
                                     !TextUtils.isEmpty(libraryInfo.folderName)
