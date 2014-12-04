695cb455d232d3b416b16c89b2b828fd3b1b535c
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2014-12-04 14:33:11 -0600

    fix leak
    Change-Id: Iedea1638eab9a2b57580b550d41f045bf1421b59

--- src/com/andrew/apollo/MultiPlayer.java
+++ src/com/andrew/apollo/MultiPlayer.java
@@ -173,6 +173,9 @@ public class MultiPlayer implements
     public void release() {
         stop();
         mCurrentMediaPlayer.release();
+        if (mNextMediaPlayer != null) {
+            mNextMediaPlayer.release();
+        }
     }
 
     /**
