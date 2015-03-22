8c0b1db4bfd389ef51660be42799bb6590263b71
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2015-03-22 15:48:43 -0500

    drawer fixup
    Change-Id: Ie73a6ea28915661abbe986b16026ee988822473f

--- src/org/opensilk/music/ui2/LauncherActivity.java
+++ src/org/opensilk/music/ui2/LauncherActivity.java
@@ -383,6 +383,7 @@ public class LauncherActivity extends BaseSwitcherToolbarActivity implements
         mDrawerToggle = new ActionBarDrawerToggle(
                 this,                    /* host Activity */
                 mDrawerLayout,                    /* DrawerLayout object */
+                mToolbar,
                 R.string.navigation_drawer_open,  /* "open drawer" description for accessibility */
                 R.string.navigation_drawer_close  /* "close drawer" description for accessibility */
         ) {
