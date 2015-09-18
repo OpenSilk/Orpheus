31db77c09f21f88de800f3dcf2e19b3baeff19db
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2015-09-17 21:02:38 -0500

    index: client stub
    Change-Id: Ie1e1b0ca1d7d322c085274126a855a24322870a9

---
+++ core-index/src/main/java/org/opensilk/music/index/client/IndexClient.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2015 OpenSilk Productions LLC
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.opensilk.music.index.client;
+
+import android.net.Uri;
+
+/**
+ * Created by drew on 9/17/15.
+ */
+public interface IndexClient {
+    /**
+     * @param uri
+     * @return True if uri or any ancestor is indexed
+     */
+    boolean isIndexed(Uri uri);
+
+    /**
+     *
+     * @param uri
+     * @return True if succeeded, false on error or if {@link #isIndexed(Uri)} returns true;
+     */
+    boolean add(Uri uri);
+
+    /**
+     *
+     * @param uri
+     * @return True if success, false on error or if {@link #isIndexed(Uri)} returns false;
+     */
+    boolean remove(Uri uri);
+}
---
+++ core-index/src/main/java/org/opensilk/music/index/client/IndexClientModule.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2015 OpenSilk Productions LLC
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.opensilk.music.index.client;
+
+import dagger.Module;
+import dagger.Provides;
+
+/**
+ * Created by drew on 9/17/15.
+ */
+@Module
+public class IndexClientModule {
+    @Provides
+    public IndexClient provideINdexClient(IndexClientImpl impl) {
+        return impl;
+    }
+}
