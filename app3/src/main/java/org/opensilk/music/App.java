9bd33939ff2b1c6d15fb2fe545350d725b9a31aa
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2015-10-31 21:35:27 -0500

    Revert "wip plugable renderers"
    welp cant change sharedUserId as orpheus is already
    on the playstore, it was a nice thought though,
    maybe will still try with multiprocess
    
    This reverts commit 23418541545eaf92abf620d769a78e08b1a6229a.

--- app3/src/main/java/org/opensilk/music/App.java
+++ app3/src/main/java/org/opensilk/music/App.java
@@ -86,7 +86,8 @@ public class App extends BaseApp {
         } else if (isProviderProcess()) {
             return ProviderComponent.FACTORY.call(this);
         } else if (isServiceProcess()) {
-            return PlaybackComponent.FACTORY.call(this);
+            //return PlaybackComponent.FACTORY.call(this);
+            return ServiceComponent.FACTORY.call(this);
         } else {
             Timber.e("Unable to determine our process");
             return EmulatorComponent.FACTORY.call(this);
@@ -94,7 +95,7 @@ public class App extends BaseApp {
     }
 
     boolean isServiceProcess() {
-        return StringUtils.endsWith(getProcName(), ".playback");
+        return StringUtils.endsWith(getProcName(), ":service");
     }
 
     boolean isUiProcess() {
