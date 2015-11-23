4660087d3ddf6e5217ee00e318dcd44131288551
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2015-11-23 15:01:42 -0600

    setup acra
    Change-Id: I3d0cc02358a7cb895e44b933571287bb7df879ff

--- app3/src/main/java/org/opensilk/music/App.java
+++ app3/src/main/java/org/opensilk/music/App.java
@@ -19,6 +19,12 @@ package org.opensilk.music;
 
 import android.content.ComponentCallbacks2;
 
+import org.acra.ACRA;
+import org.acra.ACRAConfiguration;
+import org.acra.ReportingInteractionMode;
+import org.acra.annotation.ReportsCrashes;
+import org.acra.log.HollowLog;
+import org.acra.sender.HttpSender;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.opensilk.common.core.app.BaseApp;
@@ -32,13 +38,52 @@ import java.util.List;
 import mortar.MortarScope;
 import timber.log.Timber;
 
+import static org.acra.ReportField.*;
+
+@ReportsCrashes(
+        formUri = BuildConfig.ACRA_REPORTING_URL,
+        formUriBasicAuthLogin = BuildConfig.ACRA_REPORTING_USR,
+        formUriBasicAuthPassword = BuildConfig.ACRA_REPORTING_PASS,
+        httpMethod = HttpSender.Method.PUT,
+        reportType = HttpSender.Type.JSON,
+        mode = ReportingInteractionMode.TOAST,
+        resToastText = R.string.msg_crash_report_sent,
+        sendReportsAtShutdown =  false,
+        customReportContent = {
+                REPORT_ID,
+                APP_VERSION_CODE,
+                APP_VERSION_NAME,
+                PACKAGE_NAME,
+                PHONE_MODEL,
+                ANDROID_VERSION,
+                BUILD,
+                BRAND,
+                PRODUCT,
+                TOTAL_MEM_SIZE,
+                AVAILABLE_MEM_SIZE,
+                STACK_TRACE,
+                STACK_TRACE_HASH,
+                INITIAL_CONFIGURATION,
+                CRASH_CONFIGURATION,
+                DISPLAY,
+                USER_APP_START_DATE,
+                USER_CRASH_DATE,
+                IS_SILENT,
+                INSTALLATION_ID,
+                DEVICE_FEATURES,
+                ENVIRONMENT,
+                MEDIA_CODEC_LIST,
+                THREAD_DETAILS,
+        }
+)
 public class App extends BaseApp {
-    private static final boolean DEBUG = BuildConfig.DEBUG;
+    private static final boolean ENABLE_LOGGING = BuildConfig.LOGV;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        setupTimber(DEBUG, null);
+        setupTimber(ENABLE_LOGGING, null);
+        initAcra();
         registerComponentCallbacks(mComponentCallbacks);
         enableStrictMode();
     }
@@ -61,6 +106,27 @@ public class App extends BaseApp {
         } else {
             Timber.e("Unable to determine our process");
             return EmulatorComponent.FACTORY.call(this);
+        }
+    }
+
+    void initAcra() {
+        ReportsCrashes rc = App.class.getAnnotation(ReportsCrashes.class);
+        ACRAConfiguration ac = new ACRAConfiguration(rc);
+        if (isUiProcess()) {
+            ac.setReportsDir("acra-ui-reports");
+        } else if (isServiceProcess()) {
+            ac.setReportsDir("acra-service-reports");
+        } else if (isProviderProcess()) {
+            ac.setReportsDir("acra-provider-reports");
+        } else {
+            Timber.i("Running on emulator disabling crash reporting");
+            return;
+        }
+        if (!StringUtils.isEmpty(rc.formUri())) {
+            ACRA.init(this, ac);
+            if (!ENABLE_LOGGING) {
+                ACRA.setLog(new HollowLog());
+            }
         }
     }
 
