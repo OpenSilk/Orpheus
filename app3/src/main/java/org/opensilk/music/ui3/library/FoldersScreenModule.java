8a9f179ec586639b712bc1da4883d93e32077d03
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2015-12-08 11:49:01 -0600

    process container removal in background
    Change-Id: I40cb94f9fb32e4c99d8d1ad74c99dc9d16aa026b

--- app3/src/main/java/org/opensilk/music/ui3/library/FoldersScreenModule.java
+++ app3/src/main/java/org/opensilk/music/ui3/library/FoldersScreenModule.java
@@ -18,6 +18,7 @@
 package org.opensilk.music.ui3.library;
 
 import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.net.Uri;
@@ -25,12 +26,14 @@ import android.support.v7.app.AlertDialog;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import org.opensilk.common.core.dagger2.ScreenScope;
 import org.opensilk.common.ui.mortar.ActivityResultsController;
 import org.opensilk.common.ui.mortar.DialogFactory;
 import org.opensilk.music.R;
 import org.opensilk.music.index.client.IndexClient;
+import org.opensilk.music.index.client.IndexClientImpl;
 import org.opensilk.music.library.LibraryConfig;
 import org.opensilk.music.model.Container;
 import org.opensilk.music.model.Model;
@@ -47,11 +50,20 @@ import org.opensilk.music.ui3.common.PlayAllItemClickListener;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import javax.inject.Named;
 
 import dagger.Module;
 import dagger.Provides;
+import mortar.Presenter;
+import rx.Observable;
+import rx.Subscriber;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by drew on 5/2/15.
@@ -260,13 +272,16 @@ public class FoldersScreenModule {
                             }
                         }
                         return true;
-                    case R.id.remove_from_index:
+                    case R.id.remove_from_index: {
+                        List<Container> containers = new ArrayList<>();
                         for (Model b : presenter.getSelectedItems()) {
                             if (b instanceof Container) {
-                                indexClient.remove((Container) b);
+                                containers.add((Container)b);
                             }
                         }
+                        handleContainerRemoval(presenter, containers);
                         return true;
+                    }
                     case R.id.add_to_queue: {
                         enqueueSel(presenter.getPlaybackController(), presenter.getSelectedItems(), PlaybackConstants.ENQUEUE_LAST);
                         return true;
@@ -307,6 +322,39 @@ public class FoldersScreenModule {
 
                 }
 
+            }
+
+            void handleContainerRemoval(final BundleablePresenter presenter, final List<Container> containers) {
+                Observable.create(new Observable.OnSubscribe<Boolean>() {
+                    @Override
+                    public void call(Subscriber<? super Boolean> subscriber) {
+                        boolean allsuccess = true;
+                        for (Container c : containers) {
+                            if (!presenter.getIndexClient().remove(c)){
+                                allsuccess = false;
+                            }
+                        }
+                        subscriber.onNext(allsuccess);
+                        subscriber.onCompleted();
+                    }
+                }).subscribeOn(Schedulers.computation())
+                        .delay(500, TimeUnit.MILLISECONDS)
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(new Action1<Boolean>() {
+                            @Override
+                            public void call(Boolean aBoolean) {
+                                presenter.getDialogPresenter().dismissDialog();
+                            }
+                        });
+                presenter.getDialogPresenter().showDialog(new DialogFactory() {
+                    @Override
+                    public Dialog call(Context context) {
+                        ProgressDialog d = new ProgressDialog(context);
+                        d.setIndeterminate(true);
+                        d.setMessage(context.getString(R.string.processing));
+                        return d;
+                    }
+                });
             }
 
             private void enqueueSel(PlaybackController playbackController, List<Model> selectedItems, int where) {
--- app3/src/main/java/org/opensilk/music/ui3/library/FoldersScreenView.java
+++ app3/src/main/java/org/opensilk/music/ui3/library/FoldersScreenView.java
@@ -17,24 +17,35 @@
 
 package org.opensilk.music.ui3.library;
 
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.util.AttributeSet;
+import android.widget.Toast;
 
 import com.jakewharton.rxbinding.view.RxView;
 import com.jakewharton.rxbinding.view.ViewClickEvent;
 
 import org.opensilk.common.core.mortar.DaggerService;
+import org.opensilk.common.ui.mortar.DialogFactory;
 import org.opensilk.common.ui.widget.FloatingActionButtonCheckable;
 import org.opensilk.music.R;
 import org.opensilk.music.model.Container;
 import org.opensilk.music.ui3.common.BundleableRecyclerCoordinator;
+
+import java.util.concurrent.TimeUnit;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import rx.Observable;
+import rx.Subscriber;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
+import rx.schedulers.Schedulers;
 import rx.subscriptions.CompositeSubscription;
 
 /**
@@ -90,13 +101,44 @@ public class FoldersScreenView extends BundleableRecyclerCoordinator {
                             mPresenter.getIndexClient().add(mThisContainer);
                             mFab.setChecked(true);
                         } else {
-                            mPresenter.getIndexClient().remove(mThisContainer);
-                            //TODO show toast
-                            mFab.setChecked(false);
+                            handleContainerRemoval();
                         }
                     }
                 }
         ));
+    }
+
+    void handleContainerRemoval() {
+        Subscription s = Observable.create(new Observable.OnSubscribe<Boolean>() {
+            @Override
+            public void call(Subscriber<? super Boolean> subscriber) {
+                boolean success =mPresenter.getIndexClient().remove(mThisContainer);
+                subscriber.onNext(success);
+                subscriber.onCompleted();
+            }
+        }).subscribeOn(Schedulers.computation())
+                .delay(500, TimeUnit.MILLISECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<Boolean>() {
+                    @Override
+                    public void call(Boolean aBoolean) {
+                        mPresenter.getDialogPresenter().dismissDialog();
+                        if (aBoolean) {
+                            mFab.setChecked(false);
+                            Toast.makeText(getContext(), R.string.msg_success, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+        mSubscriptions.add(s);
+        mPresenter.getDialogPresenter().showDialog(new DialogFactory() {
+            @Override
+            public Dialog call(Context context) {
+                ProgressDialog d = new ProgressDialog(context);
+                d.setIndeterminate(true);
+                d.setMessage(context.getString(R.string.processing));
+                return d;
+            }
+        });
     }
 
 }
